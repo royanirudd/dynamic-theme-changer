135d0bb935b0f3dc866e41883a683694be05f8ec
royanirudd <aniruddroy704@gmail.com>
2024-09-25 09:05:51 +0000

    Time based mood detection logic

--- lua/dynamic_theme_changer/mood_detector.lua
+++ lua/dynamic_theme_changer/mood_detector.lua
@@ -1,8 +1,21 @@
 local M = {}
 
+local function get_current_hour()
+	return tonumber(os.date("%H"))
+end
+
 function M.detect_mood()
-	-- Mood detection logic will be implemented later
-	return "neutral"
+	local hour = get_current_hour()
+
+	if hour >= 5 and hour < 12 then
+		return "energetic"
+	elseif hour >= 12 and hour < 17 then
+		return "focused"
+	elseif hour >= 17 and hour < 21 then
+		return "relaxed"
+	else
+		return "calm"
+	end
 end
 
 return M
