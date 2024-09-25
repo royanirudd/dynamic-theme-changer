048d56847cf213c0a744bf0901bb8082629a1357
royanirudd <aniruddroy704@gmail.com>
2024-09-25 09:41:03 +0000

    Updated README and user config

--- lua/dynamic_theme_changer/config.lua
+++ lua/dynamic_theme_changer/config.lua
@@ -8,6 +8,8 @@ M.defaults = {
 		calm = "nightfox",
 	},
 	update_interval = 60, -- Update theme every minute (in seconds)
+	debug = false, -- Set to true to enable debug messages
+	custom_logic = nil, -- Placeholder for custom mood detection logic
 }
 
 return M
