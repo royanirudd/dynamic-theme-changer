757bc394bc4e5be285bb799ac69e8a21be0ebfd5
royanirudd <aniruddroy704@gmail.com>
2024-10-17 07:01:51 +0000

    Updated mood logic
    Changed config.lua to use numeric keys
    Added mood_names to map numbers
    Simplified mood_detector to return number (1-4 by default)
    Updated init file

--- lua/dynamic_theme_changer/config.lua
+++ lua/dynamic_theme_changer/config.lua
@@ -2,14 +2,21 @@ local M = {}
 
 M.defaults = {
 	themes = {
-		energetic = "PaperColor",
-		focused = "gruvbox",
-		relaxed = "tokyonight",
-		calm = "nightfox",
+		[1] = "PaperColor", -- energetic
+		[2] = "gruvbox", -- focused
+		[3] = "tokyonight", -- relaxed
+		[4] = "nightfox", -- calm
 	},
 	update_interval = 60, -- Update theme every minute (in seconds)
 	debug = false, -- Set to true to enable debug messages
 	custom_logic = nil, -- Placeholder for custom mood detection logic
+}
+
+M.mood_names = {
+	[1] = "energetic",
+	[2] = "focused",
+	[3] = "relaxed",
+	[4] = "calm",
 }
 
 return M
